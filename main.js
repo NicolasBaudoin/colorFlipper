40ddc61ac833919cd0cdf6d9fd43a65939fb29cd
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2022-07-25 14:52:47 +0200

    main.js finished : generate random Number from 0 to length of the colors's array; copy/paste index.html to hex.html; start hex.js to generate random HEX colors

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.ceil(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 btn.addEventListener("click", function () {
-  console.log(document.body);
+  //get random number between 0 and 3
+  const randomNumber = getRandomNumber(0, 4);
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+  console.log(randomNumber);
 });
